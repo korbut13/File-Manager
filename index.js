a1218bfb5fe3d2e1899aeac89fd4c2c7b182c319
Svetlana Korbut <tebba@yandex.ru>
2023-06-25 20:51:34 +0200

    ref: remove index.js from main directory

--- index.js
+++
@@ -1,9 +0,0 @@
-import { argv } from 'node:process';
-
-let userName;
-
-argv.forEach((arg) => {
-  userName = arg.includes('username') ? arg.split('=')[1] : 'Svetlana'
-});
-
-console.log(`Welcome to the File Manager, ${userName}!`)
